6f3ead01ab179da7d4d17e29940669c75d188a2d
Your SouadAli10 <Souad.Ali10@gmail.com>
2018-03-25 14:16:16 +0300

    style edit

--- imports/ui/Vision.js
+++ imports/ui/Vision.js
@@ -6,18 +6,17 @@ export default () =>
                 <h2>Our Vision</h2>
                 <hr />
             </div>
-            <div className="row">
-            
-                
-                    <div className="about-text">
-                        <div><p>Razor aims to be the leading VC firm in emerging markets of South Asia.</p></div>
-                        <div><p>IMPLEMENTING THE VISION</p><br/><p>Razor aims to achieve its vision through implementing stringent oversight and
+            <div className="about-text">
+                <div className="about-1">
+                    <p>Razor aims to be the leading VC firm in emerging markets of South Asia.</p>
+                </div>
+                <div className="about-2">
+                    <p>IMPLEMENTING THE VISION</p>
+                    <p>Razor aims to achieve its vision through implementing stringent oversight and
 industry standard business practices by providing structure, guidance, and
 adequate corporate governance to its portfolio companies while ensuring full
 transparency with its investors.</p></div>
-                        
-                </div>
+
             </div>
         </div>
-        </div>
-    
+    </div>
