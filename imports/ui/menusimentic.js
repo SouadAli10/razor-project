baf78abb73f6cb7d31f04a5acb0a7d226b80fba9
Your SouadAli10 <Souad.Ali10@gmail.com>
2018-04-27 10:48:30 +0300

    smooth scroll mobile

--- imports/ui/menusimentic.js
+++ imports/ui/menusimentic.js
@@ -8,6 +8,7 @@ import Portfolio from './Portfolio';
 import Active from './Active'
 import Contact from './Contact'
 import Footer from './Footer'
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 
 class SidebarLeftOverlay extends Component {
@@ -21,47 +22,61 @@ class SidebarLeftOverlay extends Component {
         return (
             <div>
                 <Button onClick={this.toggleVisibility} className="menuButton" ><Icon name='content' /></Button>
-                
-                    <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
-                        <Menu.Item name='unhide'>
-                            <a className="page-scroll" href="#Vision" onClick={this.toggleVisibilityFalse}><Icon name='unhide' /><br />Our Vision</a>
-                        </Menu.Item>
-                        <Menu.Item name='help!'>
-                            <a className="page-scroll" href="#Why" onClick={this.toggleVisibilityFalse}><Icon name='help' /><br />Why?</a>
-                        </Menu.Item>
-                        <Menu.Item name='users'>
-                            <a className="page-scroll" href="#Team" onClick={this.toggleVisibilityFalse}><Icon name='users' /><br />Team</a>
-                        </Menu.Item>
-                        <Menu.Item name='road'>
-                            <a className="page-scroll" href="#portfolio" onClick={this.toggleVisibilityFalse}><Icon name='road' /><br />Portfolio</a>
-                        </Menu.Item>
-                        <Menu.Item name='sitemap'>
-                            <a className="page-scroll" href="#Active" onClick={this.toggleVisibilityFalse}><Icon name='sitemap' /><br />Partnerships</a>
-                        </Menu.Item>
-                        <Menu.Item name='comments'>
-                            <a className="page-scroll" href="#contact" onClick={this.toggleVisibilityFalse}><Icon name='comments' /><br />Contact</a>
-                        </Menu.Item>
-                    </Sidebar>
-                    <Sidebar.Pusher>
-                        <div className='HtmlContainer' onClick={this.toggleVisibilityFalse} >
-                            <Header />
 
+                <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
+                    <Menu.Item name='unhide'>
+                        <Link activeClass="active" className="test1 page-scroll" to="#Vision" spy={true} smooth={true} duration={1500} onClick={this.toggleVisibilityFalse}><Icon name='unhide' /> <br />Our Vision</Link>
+                    </Menu.Item>
+                    <Menu.Item name='help!'>
+                        <Link activeClass="active" className="test1 page-scroll" to="#Why" spy={true} smooth={true} duration={1500} onClick={this.toggleVisibilityFalse}><Icon name='help' /> <br />Why?</Link>
+                    </Menu.Item>
+                    <Menu.Item name='users'>
+                        <Link activeClass="active" className="test1 page-scroll" to="#Team" spy={true} smooth={true} duration={1500} onClick={this.toggleVisibilityFalse}><Icon name='users' /> <br />Team</Link>
+                    </Menu.Item>
+                    <Menu.Item name='road'>
+                        <Link activeClass="active" className="test1 page-scroll" to="#portfolio" spy={true} smooth={true} duration={1500} onClick={this.toggleVisibilityFalse}><Icon name='road' /> <br />Portfolio</Link>
+                    </Menu.Item>
+                    <Menu.Item name='sitemap'>
+                        <Link activeClass="active" className="test1 page-scroll" to="#Active" spy={true} smooth={true} duration={1500} onClick={this.toggleVisibilityFalse}><Icon name='sitemap' /> <br />Partnerships</Link>
+                    </Menu.Item>
+                    <Menu.Item name='comments'>
+                        <Link activeClass="active" className="test1 page-scroll" to="#contact" spy={true} smooth={true} duration={1500} onClick={this.toggleVisibilityFalse}><Icon name='comments' /> <br />Contact</Link>
+                    </Menu.Item>
+                </Sidebar>
+                <Sidebar.Pusher>
+                    <div className='HtmlContainer' onClick={this.toggleVisibilityFalse} >
+                        <Header />
+
+                        <Navigation />
+                        <Element name="#Vision" className="element" >
                             <Vision />
+                        </Element>
 
+                        <Element name="#Why" className="element" >
                             <Why />
+                        </Element>
 
+                        <Element name="#Team" className="element" >
                             <Team />
+                        </Element>
 
+                        <Element name="#portfolio" className="element" >
                             <Portfolio />
+                        </Element>
 
+                        <Element name="#Active" className="element" >
                             <Active />
+                        </Element>
 
+                        <Element name="#contact" className="element" >
                             <Contact />
+                        </Element>
 
-                            <Footer />
-                        </div>
-                    </Sidebar.Pusher>
-                
+                        <Footer />
+
+                    </div>
+                </Sidebar.Pusher>
+
 
             </div>
         )
